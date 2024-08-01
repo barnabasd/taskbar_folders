bbb7ebd83c8beb20e5d2f0d629b059b55c6e7d8c
Barnabás <ubarnabas.dev@gmail.com>
2024-08-01 16:51:21 +0200

    Added an icon

--- src/window.rs
+++ src/window.rs
@@ -1,10 +1,10 @@
-use windows_sys::Win32::UI::WindowsAndMessaging::{ CreateWindowExW, DispatchMessageW, GetMessageW, LoadCursorW, RegisterClassW, ShowWindow, TranslateMessage };
+use windows_sys::Win32::UI::WindowsAndMessaging::{ CreateWindowExW, DispatchMessageW, GetMessageW, LoadCursorW, LoadIconW, RegisterClassW, SendMessageW, ShowWindow, TranslateMessage, ICON_BIG, ICON_SMALL, IDI_APPLICATION, WM_SETICON };
 use windows_sys::Win32::Graphics::GdiPlus::{ GdiplusShutdown, GdiplusStartup, GdiplusStartupInput, GdiplusStartupOutput };
 use windows_sys::Win32::UI::WindowsAndMessaging::{ IDC_ARROW, SW_SHOW, WS_POPUP, MSG, WNDCLASSW };
-use windows_sys::Win32::Graphics::Gdi::{ CreateRoundRectRgn, SetWindowRgn };
+use windows_sys::Win32::Graphics::Gdi::{ CreateRoundRectRgn, SetWindowRgn, UpdateWindow };
 use windows_sys::Win32::System::LibraryLoader::GetModuleHandleW;
 use crate::util::{ get_startup_loc, to_wstring };
-use windows_sys::Win32::Foundation::TRUE;
+use windows_sys::Win32::Foundation::{LPARAM, TRUE, WPARAM};
 use lazy_static::lazy_static;
 use crate::action::Action;
 use std::ptr::null_mut;
@@ -28,9 +28,9 @@ pub unsafe fn initialize(icons: State) {
     let mut startup_output = GdiplusStartupOutput { NotificationHook: 0, NotificationUnhook: 0 };
     unsafe { GdiplusStartup(&mut gdiplus_token, &startup_input, &mut startup_output) };
     RegisterClassW(&WNDCLASSW {
+        cbClsExtra: 0, cbWndExtra: 0, hIcon: LoadIconW(GetModuleHandleW(null_mut()), IDI_APPLICATION),
         style: 0, lpfnWndProc: Some(crate::event::window_proc),
         hbrBackground: null_mut(), lpszMenuName: null_mut(),
-        cbClsExtra: 0, cbWndExtra: 0, hIcon: null_mut(),
         lpszClassName: to_wstring("window").as_ptr(),
         hCursor: LoadCursorW(null_mut(), IDC_ARROW),
         hInstance: GetModuleHandleW(null_mut()),
@@ -45,8 +45,12 @@ pub unsafe fn initialize(icons: State) {
         GetModuleHandleW(null_mut()),
         null_mut(),
     );
+    let hicon = LoadIconW(GetModuleHandleW(null_mut()), IDI_APPLICATION);
+    SendMessageW(hwnd, WM_SETICON, ICON_SMALL as WPARAM, hicon as LPARAM);
+    SendMessageW(hwnd, WM_SETICON, ICON_BIG as WPARAM, hicon as LPARAM);
     SetWindowRgn(hwnd, CreateRoundRectRgn(0, 0, width as i32, 47, 16, 16), TRUE);
     ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
     let mut msg: MSG = std::mem::zeroed();
     while GetMessageW(&mut msg, null_mut(), 0, 0) > 0 {
         TranslateMessage(&msg);
